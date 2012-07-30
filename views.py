3a5438fd075c980682e89e99b87965db2639c191
Blake C. Sutton <blake.c.sutton@gmail.com>
2012-07-30 12:41:20 -0700

    Added login and logout views, plus login_required decorator for adding ideas.

--- views.py
+++ views.py
@@ -1,10 +1,12 @@
 from django.db.models.aggregates import Count
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
-from django.template import context
 from django.template.context import RequestContext
-from forms import IdeaForm
-from models import Idea, Facet, Tag
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from forms import IdeaForm, LoginForm
+from models import Idea, Tag
+
 
 
 def idea_list(request):
@@ -55,12 +57,12 @@ def idea_detail(request, idea_id):
   
   return render_to_response('madscientist/detail.html', 
                             context_instance=RequestContext(request, context))
-
+@login_required(login_url="/madscientist/login/")
 def add_idea(request):
     """ This is a view to add an idea. Note that this duplicates functionality in the admin pages,
         however this view, unlike the administration site, can have restricted access by user, which
         I will be adding eventually. """
-    
+           
     if request.method == 'POST':
         # Bind the post request to the previously defined IdeaForm
         form = IdeaForm(request.POST)
@@ -82,3 +84,58 @@ def add_idea(request):
     
     return render_to_response('madscientist/add_idea.html',
                               context_instance=RequestContext(request, context))
+
+def logout_user(request):
+  """ This is a view which logs out the current user. """
+  logout(request)
+  return render_to_response('madscientist/success.html',
+                            context_instance=RequestContext(request))
+    
+def login_user(request):
+  """ This is a view for logging in a user of the site.
+  """
+  message = "um, hiii..."
+  
+  # If the form was submitted
+  if request.method == 'POST':
+    
+    # Bind the POST data to the form, this includes error information
+    # if it doesn't validate.
+    form = LoginForm(request.POST)
+    
+    # Valid the form info
+    if form.is_valid():
+      
+      username = form.cleaned_data['username']
+      password = form.cleaned_data['password']
+      user = authenticate(username=username, password=password)
+      
+      if user is not None:
+        if user.is_active:
+          
+          # Log in the user so their info is saved in the session.
+          login(request, user)
+          
+          return HttpResponseRedirect('/madscientist/')
+        
+        else:
+          # Disabled account
+          message = "Your account has been disabled."
+        
+      else:
+        # Incorrect username/password
+        message = "Your username or password is incorrect."
+        
+  else:
+    form = LoginForm()
+    
+  context = {'form': form,
+             'message': message}
+  
+  return render_to_response('madscientist/login.html',
+                             context_instance=RequestContext(request, context)) 
+      
+      
+      
+      
+      
