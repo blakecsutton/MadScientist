6a199724f3d0a30dc79e0562417593617b07ef46
Blake C. Sutton <blake.c.sutton@gmail.com>
2012-08-21 12:47:14 -0700

    Renamed Idea to Entry. Added creator and group to all pertinent models.

--- models.py
+++ models.py
@@ -1,6 +1,32 @@
 from django.db import models
 from datetime import datetime
-
+from django.contrib.auth.models import User
+    
+class EntryGroup(models.Model):
+  """ This is a group of Entries (a list, or a board in the context of Trello or 
+      Pinterest). All Entries which belong to this group will appear together 
+      one one tab, page, etc.
+  """
+  # The user who created the board. Only this user can add/edit/update/delete
+  # entries. Set to allow null entries so the user can be added automatically under
+  # the covers instead of explicitly.
+  creator = models.ForeignKey(User)
+  
+  # Automatically update the timestamp whenever the model is saved.
+  date_modified = models.DateField(auto_now=True)
+    
+  title = models.CharField(max_length=200)
+  # Optional shorter title which appears on the tab
+  short_title = models.CharField(max_length=200, null=True, blank=True)
+  
+  description = models.TextField(null=True, blank=True)
+  
+  # Flag for whether or not the group's contents are viewable by anyone.
+  public = models.BooleanField(default=False)
+  
+  def __unicode__(self):
+    return self.short_title
+  
 class Facet(models.Model):
     """ This is a Facet, a general category used to group tags. It is a one-level hierarchy, and 
         although there is no enforcement it is expected that there are relatively few facets.
@@ -9,8 +35,12 @@ class Facet(models.Model):
         facts can contain multiple tags. The description field is required because facets should have
         a lot of thought put into them.
     """
+    creator = models.ForeignKey(User)
+    
+    group = models.ForeignKey(EntryGroup)
+    
     name = models.CharField(max_length=100)
-    description = models.TextField()
+    description = models.TextField(blank=True, null=True)
     
     def __unicode__(self):
         return "%s" % self.name
@@ -27,37 +57,46 @@ class Tag(models.Model):
         can be attached to one idea. Every tag does not have to have an idea attached to it (supports 
         setting up a tagging system in advance) and every idea does not have to be tagged with something 
         (adds flexibility if an idea doesn't fit in the current system).
+        
+        Tags belong to exactly one EntryGroup. Although it is possible that users might want to 
+        share tags across groups, that possibilitiy is currently eclipsed by the desire to keep
+        tag clouds minimal and organized.
     """
+    creator = models.ForeignKey(User)
+    
+    group = models.ForeignKey(EntryGroup)
+    
     name = models.CharField(max_length=100)
-    facet = models.ForeignKey(Facet, null=True, blank=True, default=None)
+    
+    facet = models.ForeignKey(Facet, blank=True, null=True)
 
     def __unicode__(self):
          
         return self.name
 
-class Idea(models.Model):
-    """ This is an Idea, which consists of a title (short but identifiable) and a description,
-        which may be very long and included markup such as links to resources or inspiration
-        for the idea. There is also an optional Image field for a local image (such as a scanned
-        sketch/diagram/image, or maybe a photo of the completed project?).
-    """
-    date_created = models.DateField(default=datetime.now())
-    title = models.CharField(max_length=200)
-    description = models.TextField()
-    image = models.ImageField(blank=True, upload_to='madscientist')
     
-    tag = models.ManyToManyField(Tag, null=True, blank=True, default=None)
+class Entry(models.Model):
+  """ This is a single Entry, which belongs to one and only one Group.
+      All entries also have titles, dates, and optional tags. """
+      
+  creator = models.ForeignKey(User)
+  
+  group = models.ForeignKey(EntryGroup)
+  
+  date_created = models.DateField(auto_now_add=True)
+  
+  title = models.CharField(max_length=200)
+  
+  tags = models.ManyToManyField(Tag, null=True, blank=True, default=None)
+  
+  body = models.TextField(null=True, blank=True)
+  
+  image = models.ImageField(null=True, blank=True, upload_to='madscientist')
     
-    def __unicode__(self):
-        return self.title
-    
-    
-
-
-
-    
-    
-
+  def __unicode__(self):
+      return self.title
     
     
-    
+  class Meta:
+    verbose_name = "Entry"
+    verbose_name_plural = "Entries"
