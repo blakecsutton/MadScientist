e1ef9d4b13d55775c3f753780cbb0f85b2ec314e
Blake C. Sutton <blake.c.sutton@gmail.com>
2012-08-22 15:11:09 -0700

    Added URL for editing entries.

--- urls.py
+++ urls.py
@@ -10,6 +10,7 @@ urlpatterns += patterns('',
     (r'^login/$', 'madscientist.views.login_user'),
     (r'^logout/$', 'madscientist.views.logout_user'),
     (r'^groups/(?P<group_id>\d+)/$', 'madscientist.views.entry_list'),
-    (r'^groups/(?P<group_id>\d+)/add/$', 'madscientist.views.add_entry')
+    (r'^groups/(?P<group_id>\d+)/add/$', 'madscientist.views.edit_entry'),
+    (r'^groups/(?P<group_id>\d+)/edit/(?P<entry_id>\d+)/$', 'madscientist.views.edit_entry')
 ) 
 
