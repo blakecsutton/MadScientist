d7c8a2efaf4856d4ae5e41dabeab5ee9c739635c
Blake C. Sutton <blake.c.sutton@gmail.com>
2012-08-28 18:48:54 -0700

    Updated URLs to include the username in the root list views.

--- urls.py
+++ urls.py
@@ -13,24 +13,27 @@ Each board page is /savagerose/tutorials/ -- it lowercases words and replaces sp
 """
 urlpatterns += patterns('madscientist.views',                  
     (r'^$', 'home'),
+    
     (r'^login/$', 'login_user'),
     (r'^logout/$', 'logout_user'),
-    (r'^login-error/$', 'login_user'),
+
+    (r'^add/$', 'edit_group'),
+    (r'^edit/(?P<group_id>\d+)/$', 'edit_group'),
+    (r'^delete/(?P<group_id>\d+)/$', 'delete_group'),
     
-    (r'^groups/(?P<group_id>\d+)/$', 'entry_list'),
+    (r'^(?P<group_id>\d+)/add/$', 'edit_entry'),
+    (r'^(?P<group_id>\d+)/edit/(?P<entry_id>\d+)/$', 'edit_entry'),
+    (r'^(?P<group_id>\d+)/delete/(?P<entry_id>\d+)/$', 'delete_entry'),
     
-    (r'^groups/add/$', 'edit_group'),
-    (r'^groups/edit/(?P<group_id>\d+)/$', 'edit_group'),
-    (r'^groups/delete/(?P<group_id>\d+)/$', 'delete_group'),
+    (r'^(?P<group_id>\d+)/tags/', 'edit_tags'),
     
-    (r'^groups/(?P<group_id>\d+)/add/$', 'edit_entry'),
-    (r'^groups/(?P<group_id>\d+)/edit/(?P<entry_id>\d+)/$', 'edit_entry'),
-    (r'^groups/(?P<group_id>\d+)/delete/(?P<entry_id>\d+)/$', 'delete_entry'),
+    (r'^(?P<group_id>\d+)/facets/edit/(?P<facet_id>\d+)/', 'edit_facet'),
+    (r'^(?P<group_id>\d+)/facets/delete/(?P<facet_id>\d+)/', 'delete_facet'),
     
-    (r'^groups/(?P<group_id>\d+)/tags/', 'edit_tags'),
-    
-    (r'^groups/(?P<group_id>\d+)/facets/edit/(?P<facet_id>\d+)/', 'edit_facet'),
-    (r'^groups/(?P<group_id>\d+)/facets/delete/(?P<facet_id>\d+)/', 'delete_facet')
+    # Important for these to be last because all other patterns should be checked
+    # for matches first
+    (r'^(?P<username>\w+)/$', 'home'),
+    (r'^(?P<username>\w+)/(?P<group_id>\d+)/$', 'entry_list'),
     
 ) 
 
